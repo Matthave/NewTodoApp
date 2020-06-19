bb2622ddfb7b9f0f6ffaa0faee6427b7b75b4f46
Matt <matthave@gmail.com>
2020-06-19 12:03:30 +0200

    Updates

---
+++ precache-manifest.02ec7663bf138f97b362b915df6311cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ea5de5e78cdff352a31eef3541a811",
+    "url": "/NewTodoApp/index.html"
+  },
+  {
+    "revision": "5afaf877256ee22449a5",
+    "url": "/NewTodoApp/static/js/2.bfa266a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NewTodoApp/static/js/2.bfa266a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cc59eeda8e98925e997",
+    "url": "/NewTodoApp/static/js/main.9f99a3ad.chunk.js"
+  },
+  {
+    "revision": "7c3c675f1d4ec667fad3",
+    "url": "/NewTodoApp/static/js/runtime-main.db59cd49.js"
+  }
+]);
