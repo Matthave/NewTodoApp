b2f49488625595e891a8dbfbe6f67a87e34f384a
Matt <matthave@gmail.com>
2020-06-26 12:18:06 +0200

    Updates

---
+++ precache-manifest.62e449b111c83e0f395651d58e5eb9e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34ad1e5c042b68ba58e6b03843af104",
+    "url": "/NewTodoApp/index.html"
+  },
+  {
+    "revision": "9ab2bb7a068a6851a3a7",
+    "url": "/NewTodoApp/static/js/2.74b70372.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NewTodoApp/static/js/2.74b70372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a54a38869d8ee814494",
+    "url": "/NewTodoApp/static/js/main.1bc1b791.chunk.js"
+  },
+  {
+    "revision": "7c3c675f1d4ec667fad3",
+    "url": "/NewTodoApp/static/js/runtime-main.db59cd49.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NewTodoApp/precache-manifest.ca44f6f06b3459c71458f280b8e55038.js"
+  "/NewTodoApp/precache-manifest.62e449b111c83e0f395651d58e5eb9e3.js"
 );
 
 self.addEventListener('message', (event) => {
